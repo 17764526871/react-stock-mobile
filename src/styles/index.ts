adfd36d52f104566e34b2309a20702f5c54b3bfd
Zhijie_Zhong <1129462016@qq.com>
2024-02-11 23:55:35 +0800

    feat(UI): 环境配置以及全局样式覆盖

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,8 +1,4 @@
 import './modules/global.less';
-import commonStyles from './modules/common.module.less';
 
 /** css in ts */
-export const stylesInTypeScript = {
-  /** 按钮禁用样式 */
-  custom_disabled_button: commonStyles.custom_disabled_button,
-};
+export const themeVars = {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfigExport, loadEnv } from 'vite';
 import { viteReact } from './plugins';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [viteReact],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  base: '/mobile',
-  server: {
-    host: '0.0.0.0',
-    port: 9002,
-    proxy: {
-      '/api': {
-        target: 'https://test.intellistock.cn/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  let env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [viteReact],
+    resolve: {
+      alias: {
+        '@': '/src',
       },
     },
-  },
-});
+    base: '/mobile',
+    server: {
+      host: '0.0.0.0',
+      port: 9002,
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVE,
+          // 需要代理跨域
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+};
